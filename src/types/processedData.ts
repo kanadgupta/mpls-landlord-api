5caee2e93146ee6d3ec309fad6d78ab134e0ce95
Kanad Gupta <git@kanad.dev>
2026-03-17 10:31:03 -0700

    chore: stronger types

--- src/types/processedData.ts
+++ src/types/processedData.ts
@@ -8,9 +8,31 @@ export type Result = Record<string, SuccessResult | ErrorResult>;
 
 export type SuccessResult = {
   opendata: {
-    property: Record<string, null | number | string>;
-    applicant: Record<string, null | number | string>;
-    owner: Record<string, null | number | string>;
+    property: {
+      address: string;
+      ward: string;
+      neighborhood: string;
+      community: string;
+      precinct: string;
+      latitude: number;
+      longitude: number;
+    };
+    applicant: {
+      address: string | null;
+      email: string;
+      name: string;
+      phone: string;
+      state: string;
+      zip: string;
+    };
+    owner: {
+      address: string;
+      email: string;
+      name: string;
+      phone: string;
+      state: string;
+      zip: string;
+    };
   };
   nominatim: NominatimAggregateResult;
   success: true;
